0f91f85f7d8cf46158d4965504dc33ed21cbaeaf
brandogn <bwong928@berkeley.edu>
2023-07-23 17:06:12 -0700

    new landing page

--- sherpa/src/routes/+layout.svelte
+++ sherpa/src/routes/+layout.svelte
@@ -1,11 +1,11 @@
 <script>
-  import '../app.css';
-
+  import "../app.css"
+  // import "../font.css"
   const options = ['blog', 'q&a', 'waitlist'];
 </script>
 
 <nav class="text-black border">
-  <div class="container mx-auto p-4 flex justify-between items-center max-w-6xl">
+  <div class="container mx-auto p-4 flex justify-between items-center max-w-4xl">
     <a href="." class="text-xl font-bold">3LM</a>
     <ul class="space-x-4">
       {#each options as option (option)}
@@ -18,3 +18,5 @@
 <div class="main flex items-center flex-col p-10">
   <slot />
 </div>
+
+
--- sherpa/src/routes/+page.svelte
+++ sherpa/src/routes/+page.svelte
@@ -1,24 +1,12 @@
-<div class="content-container-full">
-<img src="./static/test.png" alt="asdf">
-  
+<div class="content-container-full  overflow-hidden">
+	<img class="scale-150 h-64   w-full object-cover" src="./test.png" alt="asdf" />
 </div>
-
 
 <div class="content-container">
-  <h1 class="text-3xl font-bold">Welcome.</h1>
-  <p>
-    Document your codebase with the click of a button. Try it out <a href="submit" class="url"
-      >here</a
-    >.
-  </p>
+	<h1 class="text-3xl font-bold">Welcome.</h1>
+	<p>
+		Document your codebase with the click of a button. Try it out <a href="submit" class="url"
+			>here</a
+		>.
+	</p>
 </div>
-
-
-<!-- 
-<div class="content-container-full">
-  <p>
-    Document your codebase with the click of a button. Try it out <a href="submit" class="url"
-      >here</a
-    >.
-  </p>
-</div> -->
